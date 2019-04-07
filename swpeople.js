2ce229a2f07388a5b8cb4a25c1cc683774548b72
Travis Johnson <TJcriss25@gmail.com>
2019-04-06 18:14:56 -0600

    Created button to add to blank card.

--- swpeople.js
+++ swpeople.js
@@ -5,6 +5,11 @@ mainContainer.className = 'container'
 
 // Sets the number of stars we wish to display
 const numStars = 100;
+// Add Blank Card Button
+let cardButtonElement = document.createElement('button');
+cardButtonElement.onclick = createCard;
+cardButtonElement.textContent = 'Add Card';
+var counter = 89;
 
 // For every star we want to display
 for (let i = 0; i < numStars; i++) {
@@ -25,7 +30,60 @@ function getRandomPosition() {
   return [randomX,randomY];
 }
 
-people.forEach((person) => {
+function createCard(){
+  console.log("CREATE CARD FIRED");
+  var person = {
+    "id": counter,
+    "name": "Faker McFakerson",
+    "height": 1.72,
+    "mass": 73,
+    "gender": "male",
+    "homeworld": "tatooine",
+    "wiki": "http://starwars.wikia.com/wiki/Luke_Skywalker",
+    "image": "https://vignette.wikia.nocookie.net/starwars/images/2/20/LukeTLJ.jpg",
+    "born": -19,
+    "bornLocation": "polis massa",
+    "died": 34,
+    "diedLocation": "ahch-to",
+    "species": "human",
+    "hairColor": "blond",
+    "eyeColor": "blue",
+    "skinColor": "light",
+    "cybernetics": "Prosthetic right hand",
+    "affiliations": [
+      "Alliance to Restore the Republic",
+      "Red Squadron",
+      "Rogue Squadron",
+      "Massassi Group",
+      "Leia Organa's team",
+      "Endor strike team",
+      "Jedi Order",
+      "Bright Tree tribe",
+      "New Republic",
+      "Resistance"
+    ],
+    "masters": [
+      "Obi-Wan Kenobi",
+      "Yoda"
+    ],
+    "apprentices": [
+      "Leia Organa",
+      "Ben Solo (along with a dozen apprentices)",
+      "Rey"
+    ],
+    "formerAffiliations": []
+  };
+  console.log({people});
+  displayPeople(person);
+  var jumpToCard = document.getElementById(person.id);
+  jumpToCard.scrollIntoView();
+  counter ++;
+}
+
+people.forEach((person) => { displayPeople(person) });
+
+function displayPeople(person) {
+  console.log('hit',person);
     let cardElement = document.createElement('div')
     let innerElement = document.createElement('div')
     let frontElement = document.createElement('div')
@@ -40,12 +98,11 @@ people.forEach((person) => {
     let eyeElement = document.createElement('h2')
     let genderElement = document.createElement('h2')
 
-
-
     cardElement.className = 'flip-card'
     innerElement.className = 'flip-card-inner'
     frontElement.className = 'flip-card-front'
     backElement.className = 'flip-card-back'
+    cardElement.id = person.id;
     frontElement.box = person.name
     imageElement.src = person.image
     nameElement.textContent = person.name
@@ -55,7 +112,6 @@ people.forEach((person) => {
     skinElement.textContent = `Skin Color: ${person.skinColor}`
     genderElement.textContent = `Gender: ${person.gender}`
     eyeElement.textContent = `Eye Color: ${person.eyeColor}`
-
 
     cardElement.appendChild(innerElement)
     innerElement.appendChild(frontElement)
@@ -69,8 +125,9 @@ people.forEach((person) => {
     backElement.appendChild(genderElement)
     backElement.appendChild(eyeElement)
 
-
     mainContainer.appendChild(cardElement)
-})
+}
 
+
+mainContainer.appendChild(cardButtonElement)
 document.body.appendChild(mainContainer)
